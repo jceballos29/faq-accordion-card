f3ec676e4c252c3cd2ae8f6c148030f893f6ef11
Juan Ceballos <jacu29@gmail.com>
2022-02-19 12:26:17 -0500

    Desktop Version Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import questions from "./utils/questions";
+
+import chevron from "./images/icon-arrow-down.svg";
+import faqImageDesktop from "./images/illustration-woman-online-desktop.svg";
+import faqImageMobile from "./images/illustration-woman-online-mobile.svg";
+import box from "./images/illustration-box-desktop.svg"
+
+import backgroundMobile from "./images/bg-pattern-mobile.svg"
+import backgroundDesktop from "./images/bg-pattern-desktop.svg"
+
+const mobile = window.innerWidth <= 860;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedQuestion, setSelectedQuestion] = useState(-1);
+
+    const handleSelectedQuestion = (id) => {
+        selectedQuestion === id
+            ? setSelectedQuestion(-1)
+            : setSelectedQuestion(id);
+    };
+
+    return (
+        <div className="App">
+            <div className="card">
+                <img src={box} alt="Box" className="card__box__desktop" />
+                <div className="card__header">
+                    <img
+                        src={mobile ? faqImageMobile : faqImageDesktop}
+                        alt="Faq Woman"
+                        className={"card__faq__image"}
+                    />
+                    <img src={mobile ? backgroundMobile : backgroundDesktop} alt="Shadow Background" className="card__header__background"/>
+                </div>
+                <div className="card__body">
+                    <h1>FAQ</h1>
+                    <ul className="card__questions">
+                        {questions.map(({ id, question, answer }) => (
+                            <li
+                                key={id}
+                                className={
+                                    selectedQuestion === id
+                                        ? "card__question__selected"
+                                        : "card__question"
+                                }
+                                onClick={() => {
+                                    handleSelectedQuestion(id);
+                                }}
+                            >
+                                <div className="card__question__header">
+                                    <h3>{question}</h3>
+                                    <img
+                                        src={chevron}
+                                        alt="Arrow"
+                                        className="card__question__icon"
+                                    />
+                                </div>
+                                <div className="card__question__body">
+                                    <p>{answer}</p>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/utils/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        id: 0,
+        question: "How many team members can I invite?",
+        answer: "Lorem ipsum dolor sit amet consectetur adipiscing elit commodo, morbi augue tristique torquent varius litora.",
+    },
+    {
+        id: 1,
+        question: "What is the maximum file upload size?",
+        answer: "No more than 2GB. All files in your account must fit your allotted storage space.",
+    },
+    {
+        id: 2,
+        question: "How do I reset my passport?",
+        answer: "Lorem ipsum dolor sit amet consectetur adipiscing elit, primis tortor tincidunt lacinia ut.",
+    },
+    {
+        id: 3,
+        question: "Can I cancel my subscription?",
+        answer: "Lorem ipsum dolor sit amet consectetur, adipiscing elit penatibus torquent.",
+    },
+    {
+        id: 4,
+        question: "Do you provide additional support?",
+        answer: "Lorem ipsum dolor sit amet consectetur adipiscing, elit interdum eleifend id ad.",
+    },
+];
+
+export default questions;
